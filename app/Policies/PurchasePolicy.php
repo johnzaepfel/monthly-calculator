e41d4336805176babccace27dc44aa04de62ac35
John Zaepfel <zaepfel@buffalo.edu>
2026-04-16 16:06:35 -0400

    Add viewAny method to PurchasePolicy for universal access

--- app/Policies/PurchasePolicy.php
+++ app/Policies/PurchasePolicy.php
@@ -7,6 +7,11 @@ use App\Models\User;
 
 class PurchasePolicy
 {
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
     public function view(User $user, Purchase $purchase): bool
     {
         return $purchase->user_id === $user->id;
